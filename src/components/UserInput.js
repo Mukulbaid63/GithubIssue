dbcd3a35d0c30dfdbd330df5ad19232abbf77328
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-02-22 18:05:43 +0545

    Update UserInput.js

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -7,7 +7,7 @@ const UserInput = ({userInput,setuserinput}) => {
     }
     return (
         <div>
-            <input  style={{fontSize:'15px',backgroundColor:'#212121',color:'white',border:'none',padding:'0px 5px'}} placeholder="Username....." onChange={userHandler}></input>
+            <input  style={{fontSize:'15px',backgroundColor:'#212121',color:'white',border:'none',borderBottom:'2px solid white',margin:'2px 5px',padding:'8px'}} placeholder="Username....." onChange={userHandler}></input>
 
             
         </div>
